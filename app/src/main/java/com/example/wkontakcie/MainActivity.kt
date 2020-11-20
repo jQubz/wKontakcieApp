6da5d6668bd94b797e52ca1b51532509e1f397bd
jakub skory <jakub.skory@atos.net>
2020-11-20 21:29:04 +0100

    Overwritten jQubz version with q84fh version... sorry

--- app/src/main/java/com/example/wkontakcie/MainActivity.kt
+++ app/src/main/java/com/example/wkontakcie/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.wkontakcie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myWebView = WebView(this)
+        setContentView(myWebView)
+        myWebView.loadUrl("https://kolobrzeska.q84fh.net/")
     }
 }
